ed825a8ea4d034f0cefe5e0131481274d71da480
Aaron Ford <aaron@aaronford.net>
2018-08-07 01:24:37 +0000

    Setup initial jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import React, {Component} from 'react';
 
-class App extends Component {
+class Navbar extends Component {
+  render() {
+    return (<nav className="navbar">
+      <a href="/" className="navbar-brand">Chatty</a>
+    </nav>)
+  }
+}
+
+class MessageList extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <main className="messages">
+        <div className="message">
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content">I won't be impressed with technology until I can download food.</span>
+        </div>
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.</div>
+      </main>
     );
   }
 }
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="chatbar">
+        <input className="chatbar-username" placeholder="Your Name (Optional)"></input>
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER"></input>
+      </footer>
+    )
+  }
+}
+
+class App extends Component {
+    render() {
+      return (
+        <div>
+        <Navbar />
+        <MessageList />
+        <Footer />
+        </div>
+      );
+    }
+  }
+
 export default App;
